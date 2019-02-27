43d2fab479aa83c5f534d37c10764bfae94b6fc3
charltonaustin <charlton.austin@gmail.com>
2019-02-27 18:48:19 -0500

    Getting it to show the first three blog posts

--- cmd/serve.go
+++ cmd/serve.go
@@ -8,6 +8,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -16,6 +19,7 @@ type blogPost struct {
 	Name        string
 	PublishDate string
 	Content     template.HTML
+
 }
 
 type archiveLinks struct {
@@ -23,11 +27,15 @@ type archiveLinks struct {
 	LinkDate string
 
 }
+
+type BlogPostPaths struct {
+	year  string
+	month string
+	day   string
+	name  string
+}
 func main() {
-	log.Printf("Listening on %v; ctrl + c to stop", ":9000")
 	router := mux.NewRouter()
-	md := []byte("## markdown document")
-	output := markdown.ToHTML(md, nil, nil)
 
 	router.Methods("GET").Path("/{year}/{month}/{day}/{name}").Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
@@ -37,7 +45,6 @@ func main() {
 		name := vars["name"]
 		content, err := ioutil.ReadFile(fmt.Sprintf("/Users/charltonaustin/dev/personal/blog-entries/published/%v/%v/%v/%v.md", yearStr, monthStr, dayStr, name))
 		if err != nil {
-			log.Printf("in error condition")
 			NotFound(w, r)
 			return
 		}
@@ -75,6 +82,41 @@ func main() {
 
 
 	router.Methods("GET").Path("/").Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var paths []BlogPostPaths
+		filepath.Walk(
+			"/Users/charltonaustin/dev/personal/blog-entries/published",
+			filepath.WalkFunc(func(path string, info os.FileInfo, err error) error {
+				if !info.IsDir() && strings.Contains(info.Name(), ".md") {
+					path = strings.Replace(path, "/Users/charltonaustin/dev/personal/blog-entries/published/", "", -1)
+					date := strings.Split(path, "/")
+					paths = append(
+						paths,
+						BlogPostPaths{
+							year:  date[0],
+							month: date[1],
+							day:   date[2],
+							name:  info.Name(),
+						},
+					)
+				}
+				return nil
+			}))
+
+		var blogPosts []blogPost
+		for i := len(paths) - 1; i > len(paths)-4; i-- {
+			blogPostInfo := paths[i]
+			content, err := ioutil.ReadFile(fmt.Sprintf("/Users/charltonaustin/dev/personal/blog-entries/published/%v/%v/%v/%v", blogPostInfo.year, blogPostInfo.month, blogPostInfo.day, blogPostInfo.name))
+			if err != nil {
+				panic(err)
+			}
+			blogPosts = append(blogPosts, blogPost{
+				Single:      "",
+				Name:        blogPostInfo.name,
+				PublishDate: fmt.Sprintf("%v/%v/%v", blogPostInfo.year, blogPostInfo.month, blogPostInfo.day),
+				Content:     template.HTML(content),
+			})
+		}
+
 		tmpl := template.Must(template.ParseFiles(
 			"templates/base.html",
 			"templates/home.html",
@@ -93,27 +135,22 @@ func main() {
 			Next			string
 			ArchiveLinks	[]archiveLinks
 		}{
-			DescriptionTag: "description tag",
+			DescriptionTag: "latest three blog posts",
 			Content:        "This is the blog page",
 			TitleTag:       "This is a title",
 			HomeActive:     "active",
 			AboutActive:    "",
-			BlogPosts: []blogPost{{
-				Single:      "single-",
-				Name:        "post name",
-				PublishDate: time.Now().Format("2006-01-02"),
-				Content:     template.HTML(output),
-			}},
-			Previous:     "/",
-			Next:         "2018/02",
-			ArchiveLinks: []archiveLinks{{LinkDate: "Feb 2018", LinkHref: "2018/02"}},
+			BlogPosts:      blogPosts,
+			Previous:       "/",
+			Next:           "2018/02",
+			ArchiveLinks:   []archiveLinks{{LinkDate: "Feb 2018", LinkHref: "2018/02"}},
 		})
 	}))
 
 	router.Methods("GET").Path("/about").Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		content, err := ioutil.ReadFile("/Users/charltonaustin/dev/personal/blog-entries/about.md")
 		if err != nil {
-			panic("unable to read about file")
+			panic(err)
 		}
 		output := markdown.ToHTML(content, nil, nil)
 		tmpl := template.Must(template.ParseFiles("templates/base.html", "templates/about.html"))
@@ -134,10 +171,10 @@ func main() {
 	}))
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/public"))))
 	router.NotFoundHandler = http.HandlerFunc(NotFound)
+	log.Printf("Listening on %v; ctrl + c to stop", ":9000")
 	http.ListenAndServe(":9000", router)
 }
 func NotFound(w http.ResponseWriter, r *http.Request) {
-	log.Printf("in not found handler")
 	tmpl := template.Must(template.ParseFiles("templates/error.html"))
 	tmpl.Execute(w, struct {
 		Title   string
